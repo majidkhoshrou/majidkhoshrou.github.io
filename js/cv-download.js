06f08d341af3e1a5b0362265a8fc66c1d5cdebad
majidkhoshrou <majid.khoshrou@alliander.com>
2025-07-08 22:29:51 +0200

    updated contact.html

--- js/cv-download.js
+++ js/cv-download.js
@@ -1,7 +1,7 @@
 document.getElementById("download-cv").addEventListener("click", () => {
   const element = document.body;
   html2pdf().set({
-    margin: 0.6,
+    margin: 0.3,
     filename: 'Majid_Khoshrou_CV.pdf',
     html2canvas: { scale: 2 },
     jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
