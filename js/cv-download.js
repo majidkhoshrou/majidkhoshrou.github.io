b480bd4589f58ed42ae7f26d40040391d80a550d
majidkhoshrou <majid.khoshrou@alliander.com>
2025-07-08 20:35:31 +0200

    updated talk and cv pages

---
+++ js/cv-download.js
@@ -0,0 +1,9 @@
+document.getElementById("download-cv").addEventListener("click", () => {
+  const element = document.body;
+  html2pdf().set({
+    margin: 0.6,
+    filename: 'Majid_Khoshrou_CV.pdf',
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+  }).from(element).save();
+});
